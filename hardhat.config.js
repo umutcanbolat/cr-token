450d12ccc190cf171d0b634420a0e0cef2f8ca5b
umutcanbolat <hello@umutcanbolat.com>
2021-10-24 00:34:09 +0200

    add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const ALCHEMY_API_KEY = "KEY";
+
+const ROPSTEN_PRIVATE_KEY = "YOUR ROPSTEN PRIVATE KEY";
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
